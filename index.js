59eb5b1ed10f840d7b58ec30514f31cf5af614c9
Alex Lee <aintaer@aintaer.com>
2014-05-13 00:06:02 -0400

    Fixing according to wintersmith conventions
    Also, adding more options

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ var path = require('path');
 var escompile = require('esnext').compile;
 var Transpiler = require('es6-module-transpiler').Compiler;
 
+var formats = {
+	'amd': 'toAMD',
+	'yui': 'toYUI',
+	'cjs': 'toCJS',
+	'globals': 'toGlobals'
+};
+
 module.exports = function pluging(env, callback) {
-	function EsCompiler(path, content, config) {
+	function EsCompiler(path, content) {
 		this.path = path;
 		this.content = content;
 	}
 
-	EsCompiler.prototype = Object.create(env.ContentPlugin);
+	EsCompiler.prototype = Object.create(env.ContentPlugin.prototype);
 	EsCompiler.prototype.constructor = EsCompiler;
 	EsCompiler.prototype.getFilename = function() {
 		return this.path.relative;
@@ -17,18 +24,23 @@ module.exports = function pluging(env, callback) {
 	EsCompiler.prototype.getModulename = function() {
 		return env.utils.stripExtension(this.getFilename());
 	};
-	EsCompiler.prototype.getView = function(env, locals, contents, templates, callback) {
-		var conf = env.config.esnext || {},
-		transpileConf = env.utils.extend({
-			compatFix: true
-		}, conf.transpilerOptions),
-		intermed = escompile(this.content, conf.compilerOptions),
-		localCompiler = new Transpiler(intermed.code, this.getModulename(), transpileConf);
-		return callback(null, new Buffer(localCompiler.toAMD()));
+	EsCompiler.prototype.getPluginColor = function() {
+		return 'yellow';
 	};
-
+	EsCompiler.prototype.getView = function() {
+		return function view(env, locals, contents, templates, callback) {
+			var config = env.config.esnext || {},
+			format = formats[config.format || 'globals'],
+			transpileConf = env.utils.extend({
+				compatFix: true
+			}, config.transpilerOptions),
+			intermed = escompile(this.content, config.compilerOptions),
+			localCompiler = new Transpiler(intermed.code, this.getModulename(), transpileConf);
+			callback(null, new Buffer(localCompiler[format]()));
+		};
+	};
 	EsCompiler.fromFile = function(path, callback) {
-		fs.readFile(path, function(err, data) {
+		fs.readFile(path.full, function(err, data) {
 			var escomp;
 			if (!err) {
 				escomp = new EsCompiler(path, data.toString());
@@ -37,6 +49,7 @@ module.exports = function pluging(env, callback) {
 		});
 	};
 
-	env.registerContentPlugin('scripts', '**/*.(js|es)', EsCompiler);
+	var config = env.config.esnext || {};
+	env.registerContentPlugin('scripts', config.pattern || '**/*.*(js|es)', EsCompiler);
 	callback();
 };
